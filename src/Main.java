af066bb643cbdc841aedcb9ec4d9977fa3eefe3d
Igor Matheus Diniz <igor.pinheiro@ccc.ufcg.edu.br>
2018-05-08 23:38:01 -0300

    MST Done, tem que ajeitar a leitura do grafo com peso

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
+
+
 /**
  * Created by viniagostini on 28/04/2018.
  */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Driele!!");
+    	System.out.println("Hello Driele!!");
         IO io = new IO();
-        System.out.println(io.readWeightedGraph("testWeightedGraph.txt"));
+        System.out.println(io.read("C:\\Users\\Igor Matheus\\Desktop\\ATG\\entradas.txt"));
+        
     }
 }
